c41dd0e41596f5cf3ca9fe0f83f2cca287bc5092
avina <avina@192.168.1.36>
2024-08-12 20:34:27 +0530

    commit 4...

---
+++ src/Day1/ArrayDec.java
@@ -0,0 +1,33 @@
+package Day1;
+
+public class ArrayDec {
+
+	public static void main(String[] args) {
+int a []; // decleration
+a = new int [5]; // initalization
+
+int b[] = new int [4];
+
+int c [] = {45,33,32,22,21,23}; // decleartion + memory allocation
+
+ //how to find a length of an array
+
+int  length = c.length;
+System.out.println(c);
+
+int num1 [] = new int [5];
+
+ num1 [0] = 1;
+ num1 [1] = 31;
+ num1 [2] = 13;
+ num1 [3] = 14;
+ num1 [4] = 44;
+ 
+ for( int i=0; i<num1.length; i++) {
+	 System.out.println(num1[i]);
+
+ 
+ }
+	}
+
+}
